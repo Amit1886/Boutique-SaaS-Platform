09b48d756f6f688348c84823c599b7a34e3684aa
Amit <kumaraj1886@gmail.com>
2026-04-09 19:56:30 +0530

    Update gradlew.bat

--- boutique_ai_saas/android_webview_app/gradlew.bat
+++ boutique_ai_saas/android_webview_app/gradlew.bat
@@ -1,10 +1,3 @@
 @echo off
-set DEFAULT_JVM_OPTS=
-
-set DIRNAME=%~dp0
-set APP_BASE_NAME=%~n0
-set APP_HOME=%DIRNAME%
-
-set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
-
-java %DEFAULT_JVM_OPTS% -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
+set DIR=%~dp0
+"%DIR%\gradle\wrapper\gradle-wrapper.jar" %*
