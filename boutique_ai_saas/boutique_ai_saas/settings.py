268cbe19f3f7240b5fd31aeb5839a884eb42d84b
Amit1886 <kumaraj1886@gmail.com>
2026-04-12 13:35:24 +0530

    Fix STATIC_URL to absolute /static/

--- boutique_ai_saas/boutique_ai_saas/settings.py
+++ boutique_ai_saas/boutique_ai_saas/settings.py
@@ -103,7 +103,7 @@ TIME_ZONE = "Asia/Kolkata"
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
